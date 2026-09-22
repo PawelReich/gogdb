25a72f34f23bbc4f37513ef3af34f5b5190898d1
Pawel Reich <gitmanik@gitmanik.dev>
2026-09-23 00:24:26 +0200

    main: Add `flatten` function

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+func flatten(value map[string]any) ([]byte, error) {
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(value)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
 func main() {
 	fmt.Println("gogdb")
 
@@ -23,6 +32,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(ret)
+	str, err := flatten(ret)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(str))
 
 }
