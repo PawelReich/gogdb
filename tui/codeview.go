967bbd9857a05fdff04d2291619f550da8ca8a1b
Pawel Reich <gitmanik@gitmanik.dev>
2026-09-24 00:00:17 +0200

    CodeView: Improve formatting

--- tui/codeview.go
+++ tui/codeview.go
@@ -63,41 +63,63 @@ func (view *CodeView) PrettyPrintCode(frame *client.GdbStackFrame) string {
 	if err != nil {
 		return fmt.Sprintf("[red::b]Could not read file: %s", frame.FilePath)
 	}
+	codeString := string(code)
 
 	lexer := lexers.Match(frame.FilePath)
 
-	iterator, err := lexer.Tokenise(nil, string(code))
+	iterator, err := lexer.Tokenise(nil, codeString)
+	tokens := iterator.Tokens()
+	lines := chroma.SplitTokensIntoLines(tokens)
 
-	for _, token := range iterator.Tokens() {
-		var colorTag string
-		switch token.Type.Category() {
-		case chroma.Keyword:
-			colorTag = "[yellow]"
-		case chroma.Name:
-			colorTag = "[cyan]"
-		case chroma.Literal:
-			colorTag = "[green]"
-		case chroma.Text:
-			colorTag = "[magenta]"
-		case chroma.Punctuation:
-			colorTag = "[white]"
-		case chroma.Comment:
-			// // Hack: Constants (e.g. 0x80e8153c  add r2, r1, #304    @ 0x130)
-			// // Are improperly tokenized as comments, force them as `Punctuation` for now
-			// if token.Value[0] == '#' {
-			// 	colorTag = "[white]"
-			// } else {
-			colorTag = "[grey]"
-			// }
-
-		default:
-			colorTag = fmt.Sprintf("[-]\n:FAILED %s = %s\n", token.Type.Category(), tview.Escape(token.Value))
-		}
-		sb.WriteString(colorTag)
-		sb.WriteString(tview.Escape(token.Value))
+	currentLine, err := strconv.Atoi(frame.FileLine)
+	if err != nil {
+		view.app.LogError(fmt.Sprintf("Error parsing file line from stack frame: %s", frame.FileLine))
+		currentLine = -1
 	}
 
-	sb.WriteString("[::B]\n")
+	totalLines := len(strings.Split(codeString, "\n"))
+	lineCounterWidth := len(strconv.Itoa(totalLines))
+
+	for line, tokens := range lines {
+		if currentLine == line {
+			sb.WriteString("[white::ib]")
+		} else {
+			sb.WriteString("[grey::i]")
+		}
+		sb.WriteString(fmt.Sprintf("%*d", lineCounterWidth, line))
+		sb.WriteString(" [::I]│[::]")
+
+		for _, token := range tokens {
+			var colorTag string
+			switch token.Type.Category() {
+			case chroma.Keyword:
+				colorTag = "[yellow]"
+			case chroma.Name:
+				colorTag = "[cyan]"
+			case chroma.Literal:
+				colorTag = "[green]"
+			case chroma.Text:
+				colorTag = "[magenta]"
+			case chroma.Punctuation:
+				colorTag = "[white]"
+			case chroma.Comment:
+				// // Hack: Constants (e.g. 0x80e8153c  add r2, r1, #304    @ 0x130)
+				// // Are improperly tokenized as comments, force them as `Punctuation` for now
+				// if token.Value[0] == '#' {
+				// 	colorTag = "[white]"
+				// } else {
+				colorTag = "[grey]"
+				// }
+
+			default:
+				colorTag = "[white]"
+				view.app.LogError(fmt.Sprintf("FAILED %s = %s\n", token.Type.Category(), tview.Escape(token.Value)))
+			}
+			sb.WriteString(colorTag)
+			sb.WriteString(tview.Escape(token.Value))
+		}
+		sb.WriteString("[::B]")
+	}
 
 	return sb.String()
 }
